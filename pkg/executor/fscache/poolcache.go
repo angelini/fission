14341b1e0aba7717aa9b3146054b266b7951118f
Scott Côté <scott.cote@hey.com>
2023-12-04 11:32:15 -0500

    Ensure funcSvcGroup is not nil

--- pkg/executor/fscache/poolcache.go
+++ pkg/executor/fscache/poolcache.go
@@ -157,7 +157,7 @@ func (c *PoolCache) service() {
 			}
 
 			// concurrency should not be set to zero and
-			//sum of specialization in progress and specialized pods should be less then req.concurrency
+			// sum of specialization in progress and specialized pods should be less then req.concurrency
 			if req.concurrency > 0 && (specializationInProgress+len(funcSvcGroup.svcs)) >= req.concurrency {
 				resp.error = ferror.MakeError(ferror.ErrorTooManyRequests, fmt.Sprintf("function '%s' concurrency '%d' limit reached.", req.function, req.concurrency))
 			} else {
@@ -238,11 +238,16 @@ func (c *PoolCache) service() {
 				}
 			}
 		case markSpecializationFailure:
-			if c.cache[req.function].svcWaiting > c.cache[req.function].queue.Len() {
-				c.cache[req.function].svcWaiting--
-				if c.cache[req.function].svcWaiting == c.cache[req.function].queue.Len() {
-					expiredRequests := c.cache[req.function].queue.Expired()
-					c.cache[req.function].svcWaiting = c.cache[req.function].svcWaiting - expiredRequests
+			funcSvcGroup, ok := c.cache[req.function]
+			if !ok || funcSvcGroup == nil {
+				break
+			}
+
+			if funcSvcGroup.svcWaiting > funcSvcGroup.queue.Len() {
+				funcSvcGroup.svcWaiting--
+				if funcSvcGroup.svcWaiting == funcSvcGroup.queue.Len() {
+					expiredRequests := funcSvcGroup.queue.Expired()
+					funcSvcGroup.svcWaiting = funcSvcGroup.svcWaiting - expiredRequests
 				}
 			}
 		case deleteValue:
